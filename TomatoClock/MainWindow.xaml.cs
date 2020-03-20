93f167b9350232c2498bae9a5165bc2874a90d28
朱元彬 <bins_123@live.com>
2020-03-20 16:42:55 +0800

    添加绑定事件和事件输入框的数字限定

--- TomatoClock/MainWindow.xaml.cs
+++ TomatoClock/MainWindow.xaml.cs
@@ -20,10 +20,51 @@ namespace TomatoClock
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double currentTime = 15;
+        private byte[] charBytesArray = { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57 };
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public void Form_Loaded(object sender, RoutedEventArgs e)
+        {
+            timeSetter.Text = currentTime.ToString();
+        }
+
+        private void btn_Start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Reset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Set_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Finish_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void timeSetter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(timeSetter.Text);
+            if (bytes.Length < 1) return;
+            for(int i=0;i<bytes.Length;i++)
+            {
+                if(!charBytesArray.Contains(bytes[i]))
+                {
+                    timeSetter.Text = currentTime.ToString();
+                    return;
+                }
+            }
+            currentTime = Convert.ToDouble(timeSetter.Text);
+        }
     }
 }
